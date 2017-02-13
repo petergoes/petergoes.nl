09815447d5a3c70314c7760514c8904a10a9831f
Peter Goes <petergoes@gmail.com>
2017-02-13 17:56:05 +0100

    fix: rerender all files on .html changes

--- tasks/content.js
+++ tasks/content.js
@@ -1,31 +1,28 @@
 const changed = require('gulp-changed');
 const frontMatter = require('front-matter');
 const gulp = require('gulp');
+const gulpIf = require('gulp-if');
 const marked = require('marked');
 const nunjucks = require('nunjucks');
 const path = require('path');
 const rename = require('gulp-rename');
 const transform = require('gulp-transform');
 
-nunjucks.configure('./src/views', {
-    autoescape: false
-});
-
 gulp.task('content', content);
+gulp.task('content:forceall', () => content({ onlyChanged: false }));
 gulp.task('content:watch', watch);
 
-function content() {
-	gulp.src('content/**/*.md')
-		.pipe(changed('dist', {extension: '.html', transform: transformPath }))
+function content({ onlyChanged = true } = {}) {
+	return gulp.src('content/**/*.md')
+		.pipe(gulpIf(onlyChanged, changed('dist', {extension: '.html', transform: transformPath })))
 		.pipe(transform(markdownToHtml))
 		.pipe(rename(nameToFolderWithIndex))
 		.pipe(gulp.dest('dist'));
 }
 
 function watch() {
-	return gulp.watch([
-			'content/**/*.md'
-		], content);
+	gulp.watch(['content/**/*.md'], ['content']);
+	gulp.watch(['src/**/*.html'], ['content:forceall']);
 }
 
 function transformPath(newPath) {
@@ -38,9 +35,12 @@ function transformPath(newPath) {
 function markdownToHtml(contents) {
 	const data = frontMatter(contents.toString());
 	const renderedHtml = marked(data.body);
-	const renderedPage = nunjucks.render(`${data.attributes.view}.html`, { content: renderedHtml });
 
-	return renderedPage;
+	nunjucks.configure('./src/views', {
+		autoescape: false
+	});
+
+	return nunjucks.render(`${data.attributes.view}.html`, { content: renderedHtml });
 }
 
 function nameToFolderWithIndex(path) {
