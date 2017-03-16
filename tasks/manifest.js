4b23ae61246fc702c83fe92f70d83eb9ccb4438a
Peter Goes <petergoes@gmail.com>
2017-03-16 13:09:10 +0100

    refactor(manifest): add manifest task to build process

--- tasks/manifest.js
+++ tasks/manifest.js
@@ -1,23 +1,40 @@
 const gulp = require('gulp');
 const fs = require('fs');
+const path = require('path');
 const { site } = require('../config/site.js');
 const { paths } = require('./config');
 
-gulp.task('manifest', manifest);
+gulp.task('manifest:build', manifest);
+gulp.task('manifest:watch', watch);
 
-function manifest(cb) {
-	const manifestContents = fs.readFileSync(paths.source.webManifest, 'utf8');
-	const manifestData = JSON.parse(manifestContents);
-	const siteData = {
-		name: site.name,
-		short_name: site.shortName,
-		description: site.description,
-		background_color: site.themeColor,
-		theme_color: site.themeColor
-	};
-	const mergedData = Object.assign({}, manifestData, siteData);
+function manifest() {
+	const manifestData = JSON.parse(fs.readFileSync(paths.source.webManifest, 'utf8'));
+	return Promise.all([
+			writeManifestFile(manifestData)
+		]);
+}
 
-	fs.writeFileSync(paths.dist.webManifest, JSON.stringify(mergedData));
+function watch() {
+	return gulp.watch([paths.source.webManifest], ['manifest:build']);
+}
 
-	cb();
+function writeManifestFile(manifestData) {
+	return new Promise((resolve, reject) => {
+		const siteData = {
+			name: site.name,
+			short_name: site.shortName,
+			description: site.description,
+			background_color: site.themeColor,
+			theme_color: site.themeColor
+		};
+		const mergedData = Object.assign({}, manifestData, siteData);
+
+		fs.writeFile(paths.dist.webManifest, JSON.stringify(mergedData), (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	})
 }
