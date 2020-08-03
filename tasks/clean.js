c46a1f5b273a2a41ba1562b50196273d71aad22a
Peter Goes <petergoes@gmail.com>
2020-08-03 09:09:45 +0200

    fix webmanifest build

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const del = require('del');
 const { paths } = require('./config')
+const fs = require('fs')
 
 gulp.task('clean', clean);
 
 function clean() {
-	return del(paths.dist.all);
+  return del(paths.dist.all)
+    .then(() => {
+      if (!fs.existsSync(paths.dist.root)){
+        fs.mkdirSync(paths.dist.root);
+      }
+    });
 }
