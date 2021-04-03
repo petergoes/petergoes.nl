52aa11dfe2c5ee91e4fe510ee5b21464dcad4b86
Peter Goes <petergoes@gmail.com>
2021-04-03 22:36:59 +0200

    Add Gravatar package and use in data

--- _data/site.js
+++ _data/site.js
@@ -1,9 +1,11 @@
+const gravatar = require('gravatar')
+
 module.exports = {
   "email": "petergoes@gmail.com",
   "name": "PeterGoes.nl",
   "shortName": "PeterGoes.nl",
   "description": "My home on the web",
-  // "gravatar": gravatar.url('petergoes@gmail.com'),
+  "gravatar": gravatar.url('petergoes@gmail.com'),
   "subtitle": "Front-end Developer",
   "themeColor": "#263238",
   "title": "Peter Goes",
