7bdcbb6a78ee52cf13ef8d83b37e5b06bfc47355
Peter Goes <petergoes@gmail.com>
2021-04-04 17:24:03 +0200

    Add highlight plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(inclusiveLangPlugin);
 
   // You can return your Config object (optional).
