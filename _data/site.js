61db59191d3607c9f1ffa045abb48526440eb62c
Peter Goes <petergoes@gmail.com>
2021-04-13 23:27:16 +0200

    Use large image for gravatar

--- _data/site.js
+++ _data/site.js
@@ -5,7 +5,7 @@ module.exports = {
   "name": "PeterGoes.nl",
   "shortName": "PeterGoes.nl",
   "description": "My home on the web",
-  "gravatar": `https:${gravatar.url('petergoes@gmail.com')}`,
+  "gravatar": `https:${gravatar.url('petergoes@gmail.com', { size: 1024 })}`,
   "subtitle": "Front-end Developer",
   "themeColor": "#263238",
   "title": "Peter Goes",
