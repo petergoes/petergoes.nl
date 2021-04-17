04f0151b9c4bc760d3a3ca9ce522acaa532902ad
Peter Goes <petergoes@gmail.com>
2021-04-17 22:01:15 +0200

    Add publication date to blog post

--- eleventy-filter-format-date.js
+++ eleventy-filter-format-date.js
@@ -1,4 +1,6 @@
 const dayjs = require('dayjs')
+const advancedFormat = require('dayjs/plugin/advancedFormat')
+dayjs.extend(advancedFormat)
 
 module.exports = function(value, format = 'DD-MM-YYYY') {
   return dayjs(value).format(format)
