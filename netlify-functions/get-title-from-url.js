f3d6e50807cbe565457f63bb253babbb37cca37b
Peter Goes <petergoes@gmail.com>
2021-04-28 09:43:02 +0200

    Match titles over multiple lines

--- netlify-functions/get-title-from-url.js
+++ netlify-functions/get-title-from-url.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-const findTitle = /<title>(.+)<\/title>/
+const findTitle = /\<title\>(.+)\<\/title\>/
 
 exports.handler = async function getTitleFromUrl(event, context) {
   const { url } = event.queryStringParameters || {}
@@ -12,13 +12,30 @@ exports.handler = async function getTitleFromUrl(event, context) {
     }
   }
 
-  const [, title] = await fetch(decodeURIComponent(url))
-    .then(res => res.text())
-    .then(html => findTitle.exec(html))
+  let response
+  let html
+  let title
+  let match
+  try {
+    html = await fetch(decodeURI(url)).then(res => res.text())
+    match = findTitle.exec(html.replace(/\n/g, ''))
+    title = match[1].trim()
 
+    if (!title) throw new Error('Title was not found')
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ title }, null, 2)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ title }, null, 2)
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        responseHtml: html,
+        error: error.message,
+        match,
+        requestUrl: decodeURI(url),
+      })
+    } 
   }
 }
