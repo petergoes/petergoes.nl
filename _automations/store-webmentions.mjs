4bd40702629e0aa6e7d8e8e109308d5c282bf1f4
Peter Goes <petergoes@gmail.com>
2021-05-24 23:59:56 +0200

    Update github action (#19)

--- _automations/store-webmentions.mjs
+++ _automations/store-webmentions.mjs
@@ -5,6 +5,10 @@ import filter from 'lodash/fp/filter.js'
 import get from 'lodash/fp/get.js'
 import forEach from 'lodash/fp/forEach.js'
 import mkdirp from 'mkdirp'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 
 const domain = 'https://www.petergoes.nl'
 const targetFolder = path.join(process.cwd(), 'mentions')
@@ -27,6 +31,7 @@ function convertResponseToJson(response) {
 
 function checkDataValidity(data) {
   if ("children" in data) return data
+
   throw new Error("Invalid webmention.io response.")
 }
 
