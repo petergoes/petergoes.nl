d1351ba936bef5faf3dc7d3f8c0d8def764a1911
Peter Goes <petergoes@gmail.com>
2021-05-29 23:49:40 +0200

    Send webmentions via netlify

--- _netlify/plugins/trigger-send-webmentions/index.js
+++ _netlify/plugins/trigger-send-webmentions/index.js
@@ -1,15 +1,43 @@
+const { spawn } = require('child_process');
 const fetch = require('node-fetch')
 
+
+
+
 module.exports = {
-  onSuccess: ({ constants }) =>     
-    fetch(
-      'https://api.github.com/repos/petergoes/petergoes.nl/dispatches',
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `token ${process.env.GITHUB_PAT}`
-        },
-        body: '{ "event_type": "Build success" }'
-      }
-    )
+  onSuccess: ({ utils }) => {
+    const { git } = utils
+
+    const changedPosts = [...git.modifiedFiles, ...git.createdFiles]
+      .filter(file => /content\/[bookmarks|likes|replies|notes]/.test(file))
+      .map(post => post.replace('content/', ''))
+      .map(post => post.replace('.md', ''))
+      .map(post => `http://petergoes.nl/${post}/`)
+      .map(postUrl => {
+        return new Promise((resolve, reject) => {
+          console.log('Post url:', postUrl)
+          const webmention = spawn('npx', ['webmention', postUrl, '--send']);
+          let stdout = ''
+          let stderr = ''
+          webmention.stdout.on('data', (data) => {
+            stdout += data
+          });
+
+          webmention.stderr.on('data', (data) => {
+            stderr += data
+          });
+
+          webmention.on('close', (code) => {
+            if (code !== 0) {
+              console.log(stderr)
+              return reject()
+            }
+            console.log(stdout)
+            resolve()
+          });
+        })
+      })
+
+      return Promise.all(changedPosts)
+  }
 }
