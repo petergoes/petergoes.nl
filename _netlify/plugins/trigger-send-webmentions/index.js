42effca9cb5699e506f09c324b129fcc72374931
Peter Goes <petergoes@gmail.com>
2021-05-30 15:43:28 +0200

    Update likes-rss and send plugin

--- _netlify/plugins/trigger-send-webmentions/index.js
+++ _netlify/plugins/trigger-send-webmentions/index.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const { spawn } = require('child_process');
 const fetch = require('node-fetch')
 
+const feedurl = 'https://www.petergoes.nl/likes/feed.xml'
+
 module.exports = {
   onSuccess: ({ utils }) => {
+
     require('dotenv-save').config()
     const { git } = utils
 
-    const changedPosts = [...git.modifiedFiles, ...git.createdFiles]
+    const changedFiles = [...git.modifiedFiles, ...git.createdFiles]
       .filter(file => /content\/[bookmarks|likes|replies|notes]/.test(file))
-      .map(post => post.replace('content/', ''))
-      .map(post => post.replace('.md', ''))
-      .map(post => `http://petergoes.nl/${post}/`)
-      .map(postUrl => 
-        fetch(
-          `https://webmention.app/check?token=${process.env.WEBMENTION_APP_TOKEN}&url=${postUrl}`,
+      
+        
+      
+      if (changedFiles.length > 0) {
+        return fetch(
+          `https://webmention.app/check?token=${process.env.WEBMENTION_APP_TOKEN}&url=${feedurl}`,
           {
-            method: 'POST'
+            method: 'GET'
           })
           .then(response => response.text())
-      )
-
-      return Promise.all(changedPosts)
-          .then(responses => responses.forEach(console.log))
+      }
   }
 }
