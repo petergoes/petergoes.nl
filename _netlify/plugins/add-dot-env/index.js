a3609efecee8142ef34e8d6c068ee43a2bf79707
Peter Goes <petergoes@gmail.com>
2021-06-07 20:35:10 +0200

    Add syndication workflow (#38)

--- _netlify/plugins/add-dot-env/index.js
+++ _netlify/plugins/add-dot-env/index.js
@@ -2,6 +2,12 @@ const fs = require('fs')
 
 module.exports = {
   onPreBuild: () => {
-    fs.writeFileSync('.env', '', { encoding: 'utf-8' })
+    try {
+      if(!fs.existsSync('.env')) {
+        fs.writeFileSync('.env', '', { encoding: 'utf-8' })
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
