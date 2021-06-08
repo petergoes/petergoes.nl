0b97f1b82f2a507792fcbacaa67a3c737ab35d89
Peter Goes <petergoes@gmail.com>
2021-06-08 20:22:13 +0200

    allow modified replies

--- _netlify/plugins/send-webmentions/index.js
+++ _netlify/plugins/send-webmentions/index.js
@@ -11,7 +11,7 @@ module.exports = {
     const { git } = utils
 
     const changedFiles = [
-      ...git.modifiedFiles.filter(file => !/content\/replies|notes/.test(file)),
+      ...git.modifiedFiles.filter(file => !/content\/notes/.test(file)),
       ...git.createdFiles
     ]
     const changedPosts = changedFiles
